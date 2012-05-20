466fc680059df5a9f12fb4e293cc0cfd2584442c
YoYo <austrieshane@gmail.com>
2012-05-20 08:38:14 -0300

    Added new build.prop value...

--- config/common.mk
+++ config/common.mk
@@ -14,6 +14,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.dataroaming=false \
     ro.media.enc.jpeg.quality=100 \
     ro.kernel.android.checkjni=0 \
+    persist.sys.composition.type=gpu
     persist.sys.camera-sound=1
 
 # packages
