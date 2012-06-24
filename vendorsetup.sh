a38e51a73aa7660d7f08a8632809b4e45721a000
Tylar Overturf <TOverturf@gmail.com>
2012-06-23 18:38:48 -0600

    Add the i9300 to vendorsetup.sh
    Change-Id: I973b027b83f27f37cd6bad75080a9cfdfab8a1d9

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,3 +2,4 @@ add_lunch_combo liquid_maguro-userdebug
 add_lunch_combo liquid_mecha-userdebug
 add_lunch_combo liquid_spyder-userdebug
 add_lunch_combo liquid_toro-userdebug
+add_lunch_combo liquid_i9300-userdebug
