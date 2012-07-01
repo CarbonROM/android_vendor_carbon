3552cf7dc2f0af17b214ba98d237425e852a4ed6
Blueharford <jsseidel1@gmail.com>
2012-07-01 16:53:49 -0700

    add tf101
    Change-Id: Ie40fc2c5c11eeb82ed8393f0cb770613dddea7cd

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,5 +3,6 @@ add_lunch_combo liquid_maserati-userdebug
 add_lunch_combo liquid_mecha-userdebug
 add_lunch_combo liquid_spyder-userdebug
 add_lunch_combo liquid_targa-userdebug
+add_lunch_combo liquid_tf101-userdebug
 add_lunch_combo liquid_toro-userdebug
 add_lunch_combo liquid_i9300-userdebug
