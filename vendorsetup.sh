d5baecda7ccc403c35605986c12516398c67078a
LiquidSmooth <liquid0624@gmail.com>
2012-07-07 22:47:43 -0400

    Adds Samsung Note (N7000)/HTC One S (Ville) To Lunch Menu
    Change-Id: I57be2e6e60e0f11b23b3df622cfd410cd42f46f1

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,5 @@
+add_lunch_combo liquid_i9300-userdebug
+add_lunch_combo liquid_n7000-userdebug
 add_lunch_combo liquid_maguro-userdebug
 add_lunch_combo liquid_maserati-userdebug
 add_lunch_combo liquid_mecha-userdebug
@@ -5,4 +7,4 @@ add_lunch_combo liquid_spyder-userdebug
 add_lunch_combo liquid_targa-userdebug
 add_lunch_combo liquid_tf101-userdebug
 add_lunch_combo liquid_toro-userdebug
-add_lunch_combo liquid_i9300-userdebug
+add_lunch_combo liquid_ville-userdebug
