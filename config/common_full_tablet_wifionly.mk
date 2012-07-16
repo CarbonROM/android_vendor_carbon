7da63be3a9bd1c0844524e081d94a88ba1f280ee
Blueharford <jsseidel1@gmail.com>
2012-07-16 05:34:52 -0700

    make compatible
    Change-Id: Ia1b8ce83f32f28ef2da3177484a75701211a0c15

--- config/common_full_tablet_wifionly.mk
+++ config/common_full_tablet_wifionly.mk
@@ -1,13 +1,6 @@
 # Inherit common Liquid stuff
 $(call inherit-product, vendor/liquid/config/common.mk)
 
-# Bring in all audio files
-include frameworks/base/data/sounds/NewAudio.mk
-
-# Extra Ringtones
-include frameworks/base/data/sounds/AudioPackageNewWave.mk
-
-# Bring in all video files
 $(call inherit-product, frameworks/base/data/videos/VideoPackage2.mk)
 
 # Default ringtone
