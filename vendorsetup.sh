ea3eccc438191616ab32ecc958a21ba6330bba5d
LiquidSmooth <liquid0624@gmail.com>
2012-07-24 13:37:21 -0400

    Update Lunch Menu Options

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,4 @@
+add_lunch_combo liquid_d2vzw-userdebug
 add_lunch_combo liquid_i9300-userdebug
 add_lunch_combo liquid_maguro-userdebug
 add_lunch_combo liquid_maserati-userdebug
@@ -7,4 +8,3 @@ add_lunch_combo liquid_targa-userdebug
 add_lunch_combo liquid_toro-userdebug
 add_lunch_combo liquid_ville-userdebug
 add_lunch_combo liquid_vivow-userdebug
-add_lunch_combo liquid_d2vzw-userdebug
