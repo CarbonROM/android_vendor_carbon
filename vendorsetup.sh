609dec409506e340f14fb2fd44dfe00603382e2d
LiquidSmooth <liquid0624@gmail.com>
2012-07-27 16:21:55 -0400

    Remove Mecha From Lunch Menu
    Change-Id: I4a6b6924a31de13df563211e057504affc6b1c2e

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,7 +3,6 @@ add_lunch_combo liquid_grouper-userdebug
 add_lunch_combo liquid_i9300-userdebug
 add_lunch_combo liquid_maguro-userdebug
 add_lunch_combo liquid_maserati-userdebug
-add_lunch_combo liquid_mecha-userdebug
 add_lunch_combo liquid_spyder-userdebug
 add_lunch_combo liquid_targa-userdebug
 add_lunch_combo liquid_toro-userdebug
