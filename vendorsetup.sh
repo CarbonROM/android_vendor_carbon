490c25fba66a08f5f514e8ce98604d8784871e0d
LiquidSmooth <liquid0624@gmail.com>
2012-09-12 21:49:05 -0400

    Add Mecha Lunch Option/Update Apns Configs

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,5 +2,6 @@ add_lunch_combo liquid_d2vzw-userdebug
 add_lunch_combo liquid_grouper-userdebug
 add_lunch_combo liquid_i9300-userdebug
 add_lunch_combo liquid_maguro-userdebug
+add_lunch_combo liquid_mecha-userdebug
 add_lunch_combo liquid_n7000-userdebug
 add_lunch_combo liquid_toro-userdebug
