3dbc80fb9ed663d7dcac779a1f72814a56ed7c35
nocoast <voteforpedro8@gmail.com>
2012-09-22 14:40:45 -0500

    insterted missing underscores. derp.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
-add_lunch combo liquid_d2att-userdebug
-add_lunch combo liquid_d2tmo-userdebug
+add_lunch_combo liquid_d2att-userdebug
+add_lunch_combo liquid_d2tmo-userdebug
 add_lunch_combo liquid_d2vzw-userdebug
 add_lunch_combo liquid_grouper-userdebug
 add_lunch_combo liquid_i9300-userdebug
