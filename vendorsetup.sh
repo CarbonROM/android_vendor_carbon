154b4ccb9df504f0a64d1f4744f48e74234797e6
LiquidSmooth <liquid0624@gmail.com>
2012-11-02 19:35:35 -0400

    Add Moar Lunch Combos Nom

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,12 +1,13 @@
 add_lunch_combo liquid_d2att-userdebug
+add_lunch_combo liquid_d2spr-userdebug
 add_lunch_combo liquid_d2tmo-userdebug
-add_lunch_combo liquid_d2usc-userdebug
 add_lunch_combo liquid_d2vzw-userdebug
 add_lunch_combo liquid_grouper-userdebug
 add_lunch_combo liquid_hercules-userdebug
 add_lunch_combo liquid_i9100-userdebug
 add_lunch_combo liquid_i9300-userdebug
 add_lunch_combo liquid_maguro-userdebug
+add_lunch_combo liquid_mecha-userdebug
 add_lunch_combo liquid_n7000-userdebug
 add_lunch_combo liquid_quincyatt-userdebug
 add_lunch_combo liquid_skyrocket-userdebug
