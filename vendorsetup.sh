9d9f353a55fc78ccc0c1d2b74e2dc02794be9556
LiquidSmooth <liquid0624@gmail.com>
2012-11-19 13:22:17 -0500

    New Official Liquid (20) Lunch Menu

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,9 +6,10 @@ add_lunch_combo liquid_d2usc-userdebug
 add_lunch_combo liquid_d2vzw-userdebug
 add_lunch_combo liquid_grouper-userdebug
 add_lunch_combo liquid_hercules-userdebug
-add_lunch_combo liquid_i9100-userdebug
 add_lunch_combo liquid_i9300-userdebug
 add_lunch_combo liquid_maguro-userdebug
+add_lunch_combo liquid_mako-userdebug
+add_lunch_combo liquid_manta-userdebug
 add_lunch_combo liquid_n7000-userdebug
 add_lunch_combo liquid_n7100-userdebug
 add_lunch_combo liquid_quincyatt-userdebug
