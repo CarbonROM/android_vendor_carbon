2ee3396040b9cdc47c279ed937c4f9a9002e9933
liquid0624 <rjbirnesser@gmail.com>
2012-12-22 16:08:15 -0500

    Cleanup Common Configs/Remove Unwanted Prebuilts

--- config/common.mk
+++ config/common.mk
@@ -8,8 +8,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
     ro.com.google.clientidbase=android-google \
     ro.com.android.wifi-watchlist=GoogleGuest \
-    ro.com.android.dateformat=MM-dd-yyyy \
     ro.setupwizard.enterprise_mode=1 \
+    ro.com.android.dateformat=MM-dd-yyyy \
     ro.com.android.dataroaming=false \
     ro.media.enc.jpeg.quality=100 \
     ro.kernel.android.checkjni=0 \
@@ -44,10 +44,6 @@ include vendor/liquid/config/theme_chooser.mk
 # overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/liquid/overlay/common
 
-# languages
-PRODUCT_COPY_FILES += \
-vendor/liquid/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so \
-
 # bin
 PRODUCT_COPY_FILES += \
     vendor/liquid/prebuilt/common/bin/sysinit:system/bin/sysinit
@@ -56,30 +52,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/liquid/prebuilt/common/etc/sysctl.conf:system/etc/sysctl.conf
 
-# chron
-PRODUCT_COPY_FILES += \
-    vendor/liquid/prebuilt/common/etc/cron/cron.conf:system/etc/cron/cron.conf \
-    vendor/liquid/prebuilt/common/etc/cron/cron.hourly/00drop_caches:system/etc/cron/cron.hourly/00drop_caches \
-    vendor/liquid/prebuilt/common/etc/cron/cron.daily/00drop_caches:system/etc/cron/cron.daily/00drop_caches \
-    vendor/liquid/prebuilt/common/etc/cron/cron.weekly/00drop_caches:system/etc/cron/cron.weekly/00drop_caches \
-    vendor/liquid/prebuilt/common/etc/cron/cron.hourly/01clear_cache:system/etc/cron/cron.hourly/01clear_cache \
-    vendor/liquid/prebuilt/common/etc/cron/cron.daily/01clear_cache:system/etc/cron/cron.daily/01clear_cache \
-    vendor/liquid/prebuilt/common/etc/cron/cron.weekly/01clear_cache:system/etc/cron/cron.weekly/01clear_cache
-
 # initd
 PRODUCT_COPY_FILES += \
-    vendor/liquid/prebuilt/common/etc/liberty.bsh:system/etc/liberty.bsh \
-    vendor/liquid/prebuilt/common/etc/init.d/00check:system/etc/init.d/00check \
-    vendor/liquid/prebuilt/common/etc/init.d/01zipalign:system/etc/init.d/01zipalign \
-    vendor/liquid/prebuilt/common/etc/init.d/02sysctl:system/etc/init.d/02sysctl \
-    vendor/liquid/prebuilt/common/etc/init.d/03firstboot:system/etc/init.d/03firstboot \
-    vendor/liquid/prebuilt/common/etc/init.d/05freemem:system/etc/init.d/05freemem \
-    vendor/liquid/prebuilt/common/etc/init.d/06removecache:system/etc/init.d/06removecache \
-    vendor/liquid/prebuilt/common/etc/init.d/07fixperms:system/etc/init.d/07fixperms \
-    vendor/liquid/prebuilt/common/etc/init.d/09cron:system/etc/init.d/09cron \
-    vendor/liquid/prebuilt/common/etc/init.d/10sdboost:system/etc/init.d/10sdboost \
-    vendor/liquid/prebuilt/common/etc/init.d/98tweaks:system/etc/init.d/98tweaks \
-    vendor/liquid/prebuilt/common/etc/init_trigger.disabled:system/etc/init_trigger.disabled
+    vendor/liquid/prebuilt/common/etc/init.d/98tweaks:system/etc/init.d/98tweaks
 
 # prebuilt
 PRODUCT_COPY_FILES += \
@@ -89,20 +64,25 @@ PRODUCT_COPY_FILES += \
     vendor/liquid/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
     vendor/liquid/prebuilt/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
 
+# keyboard
+PRODUCT_COPY_FILES += \
+    vendor/liquid/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
+
 # sip/voip
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
 # nfc
 PRODUCT_COPY_FILES += \
+    vendor/liquid/config/permissions/com.liquidsmooth.android.xml:system/etc/permissions/com.liquidsmooth.android.xml \
     vendor/liquid/config/permissions/com.liquidsmooth.nfc.enhanced.xml:system/etc/permissions/com.liquidsmooth.nfc.enhanced.xml
 
 # version
-PRODUCT_VERSION_MAJOR = v2.1
-PRODUCT_VERSION_MINOR = 0
-PRODUCT_VERSION_MAINTENANCE = "Alpha6"
-LiquidVersion = "Liquid-JB"-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(PRODUCT_VERSION_MAINTENANCE)
+LIQUID_VERSION_MAJOR = 2
+LIQUID_VERSION_MINOR = 1
+LIQUID_VERSION_STATE = "Beta1"
+LIQUID_VERSION := "Liquid-JB-v"$(LIQUID_VERSION_MAJOR).$(LIQUID_VERSION_MINOR)-$(LIQUID_VERSION_STATE)
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.modversion=$(LiquidVersion)
+  ro.liquid.version=$(LIQUID_VERSION)
 
--- config/common_drm_tablet.mk
+++ config/common_drm_tablet.mk
@@ -1,8 +1,5 @@
 # Blobs necessary for drm
 PRODUCT_COPY_FILES +=  \
-    vendor/liquid/prebuilt/common_drm/com.google.widevine.software.drm.xml:system/etc/permissions/com.google.widevine.software.drm.xml \
-    vendor/liquid/prebuilt/common_drm/com.google.widevine.software.drm.jar:system/framework/com.google.widevine.software.drm.jar \
-    vendor/liquid/prebuilt/common_drm/tablet/lib/libfrsdk.so:system/lib/libfrsdk.so \
     vendor/liquid/prebuilt/common_drm/tablet/lib/libWVphoneAPI.so:system/lib/libWVphoneAPI.so \
     vendor/liquid/prebuilt/common_drm/tablet/vendor/lib/libwvdrm_L3.so:system/vendor/lib/libwvdrm_L3.so \
     vendor/liquid/prebuilt/common_drm/tablet/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
