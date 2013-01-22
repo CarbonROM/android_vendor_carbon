4fc8a756eb41ae61221514cb3d6fcf0bcb2416f4
Liquid <rjbirnesser@gmail.com>
2013-01-22 14:19:54 -0500

    Revert NovaLauncher/Bump Build Version/Add New Lunch Items

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,12 +6,13 @@ add_lunch_combo liquid_d2tmo-userdebug
 add_lunch_combo liquid_d2usc-userdebug
 add_lunch_combo liquid_d2vzw-userdebug
 add_lunch_combo liquid_grouper-userdebug
+add_lunch_combo liquid_i605-userdebug
 add_lunch_combo liquid_i9300-userdebug
+add_lunch_combo liquid_l900-userdebug
 add_lunch_combo liquid_maguro-userdebug
 add_lunch_combo liquid_mako-userdebug
 add_lunch_combo liquid_manta-userdebug
 add_lunch_combo liquid_n7100-userdebug
 add_lunch_combo liquid_t0lte-userdebug
-add_lunch_combo liquid_t0ltecdma-userdebug
 add_lunch_combo liquid_toro-userdebug
 add_lunch_combo liquid_toroplus-userdebug
