26270630cb396ae125077f94d405ec2134c62ad3
nocoast <voteforpedro8@gmail.com>
2013-02-06 10:28:51 -0600

    Rework the n7000 vendor commit, removing prebuitl SSM
    Because we build SSM at source no need for a prebuilt,
    besides that this looks great.

--- config/common_sgs.mk
+++ config/common_sgs.mk
@@ -1,6 +1,2 @@
 #Device Settings for Samsung Galaxy phones
 PRODUCT_PACKAGE_OVERLAYS += vendor/carbon/overlay/sgs
-
-#samsung service mode for device settings
-PRODUCT_COPY_FILES += \
-    vendor/carbon/prebuilt/common/app/SamsungServiceMode.apk:system/app/SamsungServiceMode.apk
