7b5f2a269d92145d817bbb19b22bdf9dc6118a7c
andros <andros2011@gmail.com>
2013-04-26 17:48:37 -0400

    Inherit languages from common.mk

--- config/common.mk
+++ config/common.mk
@@ -64,6 +64,9 @@ PRODUCT_PACKAGES += \
     ssh-keygen \
     start-ssh
 
+# languages
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
 # themes
 include vendor/carbon/config/theme_chooser.mk
 
