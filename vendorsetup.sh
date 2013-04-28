557d09b594e22b7ee2879f51d6ca9e637df1751c
dhacker29 <davidhackerdvm@gmail.com>
2013-04-27 22:46:43 -0500

    Add moto devices

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -25,11 +25,15 @@ add_lunch_combo carbon_n7000-userdebug
 add_lunch_combo carbon_n7100-userdebug
 add_lunch_combo carbon_quincytmo-userdebug
 add_lunch_combo carbon_skyrocket-userdebug
+add_lunch_combo carbon_solana-userdebug
+add_lunch_combo carbon_spyder-userdebug
 add_lunch_combo carbon_t0lte-userdebug
 add_lunch_combo carbon_t0lteatt-userdebug
 add_lunch_combo carbon_t0ltetmo-userdebug
+add_lunch_combo carbon_targa-userdebug
 add_lunch_combo carbon_tilapia-userdebug
 add_lunch_combo carbon_toro-userdebug
 add_lunch_combo carbon_toroplus-userdebug
+add_lunch_combo carbon_umts_spyder-userdebug
 add_lunch_combo carbon_ville-userdebug
 
