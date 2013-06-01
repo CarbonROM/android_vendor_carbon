7e56210dc8d5f60008dda611e2fbd2e1f387f95c
Derek Gelinas <mrdgelinas@gmail.com>
2013-05-31 23:39:13 -0300

    Update vendorsetup.sh
    add s4vzw to lunch.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -18,6 +18,7 @@ add_lunch_combo carbon_infuse4g-userdebug
 add_lunch_combo carbon_i9100-userdebug
 add_lunch_combo carbon_i9300-userdebug
 add_lunch_combo carbon_i9305-userdebug
+add_lunch_combo carbon_jfltevzw-userdebug
 add_lunch_combo carbon_l900-userdebug
 add_lunch_combo carbon_m7ul-userdebug
 add_lunch_combo carbon_m7tmo-userdebug
