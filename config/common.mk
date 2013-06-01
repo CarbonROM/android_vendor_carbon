001086bc73d7cccbfbc008d0e6fd05a8118f84f3
Derek Gelinas <mrdgelinas@gmail.com>
2013-06-01 11:29:36 -0300

    I think this is what might have broken the nightly notifications in goo.
    If so, we'll have to tweak it further because we kind of needed that.

--- config/common.mk
+++ config/common.mk
@@ -116,21 +116,21 @@ ifdef CARBON_NIGHTLY
     PRODUCT_PROPERTY_OVERRIDES += \
         ro.goo.rom=carbonjb2exp \
         ro.goo.developerid=carbon \
-        ro.goo.version=$(shell date +%Y%m%d%H%M)
+        ro.goo.version=$(shell date +%Y%m%d)
 endif
 ifdef CARBON_EXPERIMENTAL
     CARBON_BUILDTYPE := EXPERIMENTAL
     PRODUCT_PROPERTY_OVERRIDES += \
         ro.goo.rom=carbonjb2exp \
         ro.goo.developerid=carbon \
-        ro.goo.version=$(shell date +%Y%m%d%H%M)
+        ro.goo.version=$(shell date +%Y%m%d)
 endif
 ifdef CARBON_RELEASE
     CARBON_BUILDTYPE := RELEASE
     PRODUCT_PROPERTY_OVERRIDES += \
         ro.goo.rom=carbonjb2 \
         ro.goo.developerid=carbon \
-        ro.goo.version=$(shell date +%Y%m%d%H%M)
+        ro.goo.version=$(shell date +%Y%m%d)
 endif
 #Set Unofficial if no buildtype set (Buildtype should ONLY be set by Carbon Devs!)
 ifdef CARBON_BUILDTYPE
