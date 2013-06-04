90605e0a47021fa1b867aa92249cb77b1c10d872
arrrghhh <arrrghhh@xdandroid.com>
2013-06-03 18:26:40 -0600

    Add xt925 to lunch

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -49,3 +49,4 @@ add_lunch_combo carbon_umts_spyder-userdebug
 add_lunch_combo carbon_ville-userdebug
 add_lunch_combo carbon_xt897-userdebug
 add_lunch_combo carbon_xt897c-userdebug
+add_lunch_combo carbon_xt925-userdebug
