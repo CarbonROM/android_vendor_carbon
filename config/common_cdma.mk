5c7618a28a2b62482d7ae408ee4525041f508353
Derek Gelinas <mrdgelinas@gmail.com>
2013-06-26 01:08:02 -0300

    This overrides GPS.conf settings on a device level... no bueno.

--- config/common_cdma.mk
+++ config/common_cdma.mk
@@ -1,6 +1,6 @@
 # gps
-PRODUCT_COPY_FILES += \
-    vendor/carbon/prebuilt/common/etc/gps-cdma.conf:system/etc/gps.conf
+#PRODUCT_COPY_FILES += \
+#    vendor/carbon/prebuilt/common/etc/gps-cdma.conf:system/etc/gps.conf
 
 # apns
 PRODUCT_COPY_FILES += \
