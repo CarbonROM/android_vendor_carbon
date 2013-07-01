SlimRoms/vendor_slim

[1/2] LCD density backup
Change-Id: I2b8c7aa2419b689d5bb98a02e3c68dd2e5e0d527

config/common.mk
@@ -14,7 +14,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     vendor/slim/prebuilt/common/bin/backuptool.sh:system/bin/backuptool.sh \
     vendor/slim/prebuilt/common/bin/backuptool.functions:system/bin/backuptool.functions \
-    vendor/slim/prebuilt/common/bin/50-slim.sh:system/addon.d/50-slim.sh
+    vendor/slim/prebuilt/common/bin/50-slim.sh:system/addon.d/50-slim.sh \
+    vendor/slim/prebuilt/common/bin/99-backup.sh:system/addon.d/99-backup.sh \
+    vendor/slim/prebuilt/common/etc/backup.conf:system/etc/backup.conf
 
 # SLIM-specific init file
 PRODUCT_COPY_FILES += \

CarbonROM/android_vendor_carbon

Conflicts:
	config/common.mk

config/common.mk
@@ -100,7 +100,9 @@ PRODUCT_COPY_FILES += \
     vendor/carbon/prebuilt/common/bin/backuptool.sh:system/bin/backuptool.sh \
     vendor/carbon/prebuilt/common/bin/backuptool.functions:system/bin/backuptool.functions \
     vendor/carbon/prebuilt/common/bin/50-carbon.sh:system/addon.d/50-carbon.sh \
-    vendor/carbon/prebuilt/common/bin/blacklist:system/addon.d/blacklist
+    vendor/carbon/prebuilt/common/bin/blacklist:system/addon.d/blacklist \
+    vendor/carbon/prebuilt/common/bin/99-backup.sh:system/addon.d/99-backup.sh \
+    vendor/carbon/prebuilt/common/etc/backup.conf:system/etc/backup.conf
 
 # sip/voip
 PRODUCT_COPY_FILES += \