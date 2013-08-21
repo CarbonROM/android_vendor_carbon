3cc29a133f86f9791315747a6f7ed8b036c329ae
William Roberts <wroberts@tresys.com>
2013-08-21 13:43:54 -0700

    Use PRODUCT_BOOTANIMATION instead of COPY_FILES
    PS2: fix superuser removal, derp
    
    Change-Id: I10f6db5687ba2a3724a00b94bc7592dbd6405b82

--- config/common.mk
+++ config/common.mk
@@ -46,8 +46,7 @@ $(eval TARGET_BOOTANIMATION_NAME := $(shell \
 endef
 $(foreach size,$(bootanimation_sizes), $(call check_and_set_bootanimation,$(size)))
 
-PRODUCT_COPY_FILES += \
-    vendor/carbon/prebuilt/common/bootanimation/$(TARGET_BOOTANIMATION_NAME).zip:system/media/bootanimation.zip
+PRODUCT_BOOTANIMATION := vendor/carbon/prebuilt/common/bootanimation/$(TARGET_BOOTANIMATION_NAME).zip
 endif
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
