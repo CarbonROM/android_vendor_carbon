a62fe974805b53dad5836f030a4dc0e98fe31cf8
Andros Rosa <andros2011@gmail.com>
2013-08-27 19:30:40 -0700

    Do not add default BT configurations
    CM did this recently so I figured we should follow along since this came from them originally
    
    Change-Id: I1bcee310b07526e7163aec9da02a62fe027fbdd8

--- config/common_phone.mk
+++ config/common_phone.mk
@@ -4,10 +4,6 @@ include frameworks/base/data/sounds/NewAudio.mk
 # common
 $(call inherit-product, vendor/carbon/config/common.mk)
 
-# bluetooth
-PRODUCT_COPY_FILES += \
-    system/bluetooth/data/main.conf:system/etc/bluetooth/main.conf
-
 # media
 #PRODUCT_COPY_FILES += \
 #    vendor/carbon/prebuilt/common/media/xhdpi/bootanimation.zip:system/media/bootanimation.zip
--- config/common_tablet.mk
+++ config/common_tablet.mk
@@ -8,10 +8,6 @@ $(call inherit-product, vendor/carbon/config/common.mk)
 #PRODUCT_COPY_FILES += \
 #    vendor/carbon/prebuilt/common/etc/gps.conf:system/etc/gps.conf
 
-# bluetooth
-PRODUCT_COPY_FILES += \
-    system/bluetooth/data/main.nonsmartphone.conf:system/etc/bluetooth/main.conf
-
 # media
 #PRODUCT_COPY_FILES += \
 #    vendor/carbon/prebuilt/common/media/xhdpi/bootanimation.zip:system/media/bootanimation.zip
