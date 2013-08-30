96e68d14d1a63d32050b02c795499d3c1a2b4153
winner00 <toverturf@gmail.com>
2013-08-29 21:08:18 -0600

    Add deb

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,6 +8,7 @@ add_lunch_combo carbon_d2tmo-userdebug
 add_lunch_combo carbon_d2usc-userdebug
 add_lunch_combo carbon_d2vzw-userdebug
 add_lunch_combo carbon_d710-userdebug
+add_lunch_combo carbon_deb-userdebug
 add_lunch_combo carbon_endeavoru-userdebug
 add_lunch_combo carbon_evita-userdebug
 add_lunch_combo carbon_find5-userdebug
