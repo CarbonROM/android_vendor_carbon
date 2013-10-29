b15f08eb690522f481f68b6cb1be57d3367785da
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-10-29 17:48:25 +0000

    Don't assume phones are the only clients for the telephony configuration bits
    Data-enabled tablets need love, too
    
    Change-Id: I239823f198260d0f045c6e7305cf04082a8ef1e9

--- config/common_phone.mk
+++ config/common_phone.mk
@@ -10,8 +10,7 @@ $(call inherit-product, vendor/carbon/config/common.mk)
 
 # packages
 PRODUCT_PACKAGES += \
-  Mms \
-  Stk
+  Mms
 
 # ringtones
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -19,10 +18,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.notification_sound=Proxima.ogg \
     ro.config.alarm_alert=Cesium.ogg
 
-# World APN list
-PRODUCT_COPY_FILES += \
-    vendor/carbon/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
-
-# World SPN overrides list
-PRODUCT_COPY_FILES += \
-    vendor/carbon/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
+$(call inherit-product, vendor/carbon/config/telephony.mk)
