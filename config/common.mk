f146ca53f82cd8152ae596a81d8688de4fd6f2b3
Steve Kondik <shade@chemlab.org>
2013-11-06 13:55:11 -0800

    cm: Allow devices to set the GMS client id
    Change-Id: I6fdf3817a292ac64291e8f9506eddd7d33059344
    
    cm: Actually add the custom clientidbase
    
    Change-Id: I8105f2cd13c26777f59f1926f378f52c1152cd0d

--- config/common.mk
+++ config/common.mk
@@ -4,12 +4,18 @@ PRODUCT_BRAND ?= Carbon
 #SuperUser
 SUPERUSER_EMBEDDED := true
 
-# overrides
+ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=android-google
+else
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
+endif
+
 PRODUCT_PROPERTY_OVERRIDES += \
     keyguard.no_require_sim=true \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
     ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
-    ro.com.google.clientidbase=android-google \
     ro.com.android.wifi-watchlist=GoogleGuest \
     ro.setupwizard.enterprise_mode=1 \
     ro.com.android.dateformat=MM-dd-yyyy \
