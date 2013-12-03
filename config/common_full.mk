d1678d4a01d6981ce31eb34e25a46c6249957a4e
Boyd <irvinmouritsen@gmail.com>
2013-12-03 14:05:01 -0800

    Don't inherit none existing video makefiles
    These were removed and building breaks without this being removed as well.
    
    Change-Id: Ib6d35c20373ab4bacb2ac1e4547fd9255f8a1a2e

--- config/common_full.mk
+++ config/common_full.mk
@@ -7,9 +7,6 @@ include frameworks/base/data/sounds/NewAudio.mk
 # Extra Ringtones
 include frameworks/base/data/sounds/AudioPackageNewWave.mk
 
-# Bring in all video files
-$(call inherit-product, frameworks/base/data/videos/VideoPackage2.mk)
-
 # Optional Carbon packages
 PRODUCT_PACKAGES += \
     HoloSpiralWallpaper \
