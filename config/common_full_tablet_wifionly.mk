e99da45791d91f9ff6fe8739baf47c2effa19745
Boyd <irvinmouritsen@gmail.com>
2013-12-03 14:02:18 -0800

    Remove none existing system/bluetooth/main.conf from config
    This is gone in KitKat and causes some errors at build time. This fixes building.

--- config/common_full_tablet_wifionly.mk
+++ config/common_full_tablet_wifionly.mk
@@ -7,10 +7,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.notification_sound=Deneb.ogg \
     ro.config.alarm_alert=Hassium.ogg
 
-# BT config
-PRODUCT_COPY_FILES += \
-    system/bluetooth/data/main.nonsmartphone.conf:system/etc/bluetooth/main.conf
-
 ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
     PRODUCT_COPY_FILES += \
 # Commenting out because we use the frameworks/base boot animation
