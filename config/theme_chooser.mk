84ba623ae721a04459213bb4c09ce15a1cf837c7
dhacker29 <dhackerdvm@gmail.com>
2014-05-02 20:20:39 -0400

    cm-11 themes: Build new ThemesProvider
    Change-Id: Ia309407d94ceb38d56b899d1801944d5bda1acda
    
    also included:
    
    CM11 Themes: Add feature for new themes.
    
    Change-Id: I0da30606534c13a43de33bef08f79fbbb1b348d3

--- config/theme_chooser.mk
+++ config/theme_chooser.mk
@@ -1,10 +1,8 @@
-# packages
+# Theme engine
 PRODUCT_PACKAGES += \
-       ThemeManager \
-       ThemeChooser \
-       com.tmobile.themes
+    ThemeChooser \
+    ThemesProvider
 
-# theme chooser
 PRODUCT_COPY_FILES += \
-       vendor/carbon/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml
-
+    vendor/carbon/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
+    vendor/carbon/config/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml
