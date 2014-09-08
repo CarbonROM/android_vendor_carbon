85bc41dc4c7211ac9691bd8222b85e3a82a07825
John Kirk <jkirk777@gmail.com>
2014-09-07 20:59:28 -0500

    Add hltespr to vendorsetup.sh
    Change-Id: I27ed298d52053286d03a95ad093de149bc3a1d71

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -11,6 +11,7 @@ add_lunch_combo carbon_grouper-userdebug
 add_lunch_combo carbon_hammerhead-userdebug
 add_lunch_combo carbon_hercules-userdebug
 add_lunch_combo carbon_hlte-userdebug
+add_lunch_combo carbon_hltespr-userdebug
 add_lunch_combo carbon_i605-userdebug
 add_lunch_combo carbon_i9100-userdebug
 add_lunch_combo carbon_i9300-userdebug
