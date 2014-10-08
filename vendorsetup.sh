d7686e652e91c0ab400fdef807e92f6622e9ef79
mattmanwrx <mbloomfield87@gmail.com>
2014-10-08 11:51:59 +0100

    vendorsetup: add hlteusc
    Change-Id: I693d8b3b9733406e771c95b38b66fb67f4dc2031
    Signed-off-by: mattmanwrx <mbloomfield87@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -13,6 +13,7 @@ add_lunch_combo carbon_hammerhead-userdebug
 add_lunch_combo carbon_hercules-userdebug
 add_lunch_combo carbon_hlte-userdebug
 add_lunch_combo carbon_hltespr-userdebug
+add_lunch_combo carbon_hlteusc-userdebug
 add_lunch_combo carbon_hltevzw-userdebug
 add_lunch_combo carbon_i605-userdebug
 add_lunch_combo carbon_i9100-userdebug
