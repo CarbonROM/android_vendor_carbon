b415c5d924daccf1f54022ea4f9804ca91e71cb0
Andros Rosa <andros2011@gmail.com>
2014-10-19 20:42:56 -0400

    vendorsetup: add ktle spr/usc/vzw variants
    also cleanup dropped / unused devices.
    
    Change-Id: I4b925297686fc7cafea6c4402d13afc845c52d95

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,5 @@
 add_lunch_combo carbon_bacon-userdebug
-add_lunch_combo carbon_d2dcm-userdebug
 add_lunch_combo carbon_d2lte-userdebug
-add_lunch_combo carbon_d800-userdebug
-add_lunch_combo carbon_d801-userdebug
-add_lunch_combo carbon_d802-userdebug
 add_lunch_combo carbon_deb-userdebug
 add_lunch_combo carbon_endeavoru-userdebug
 add_lunch_combo carbon_exhilarate-userdebug
@@ -23,8 +19,10 @@ add_lunch_combo carbon_i9500-userdebug
 add_lunch_combo carbon_infuse4g-userdebug
 add_lunch_combo carbon_jflte-userdebug
 add_lunch_combo carbon_klte-userdebug
+add_lunch_combo carbon_kltespr-userdebug
+add_lunch_combo carbon_klteusc-userdebug
+add_lunch_combo carbon_kltevzw-userdebug
 add_lunch_combo carbon_l900-userdebug
-add_lunch_combo carbon_ls980-userdebug
 add_lunch_combo carbon_m7-userdebug
 add_lunch_combo carbon_m7spr-userdebug
 add_lunch_combo carbon_m7vzw-userdebug
@@ -43,7 +41,5 @@ add_lunch_combo carbon_skyrocket-userdebug
 add_lunch_combo carbon_t0lte-userdebug
 add_lunch_combo carbon_t769-userdebug
 add_lunch_combo carbon_taoshan-userdebug
-add_lunch_combo carbon_tf101-userdebug
 add_lunch_combo carbon_toro-userdebug
 add_lunch_combo carbon_toroplus-userdebug
-add_lunch_combo carbon_vs980-userdebug
