0779d37ad565b00c07333ddf9b8ba580269bd05c
STELIX <ssspinni@gmail.com>
2015-01-19 22:27:57 +0100

    Carbon - Clean up
    Orig.Change-Id: I5cb4e3977e8a4f0293bdfd44dda9fd0815ee6b96
    Signed-off-by: STELIX <ssspinni@gmail.com>
    
    Change-Id: I8cbdf6c7bfa7bd7682152b27137a0e864919bd1d

---
+++ config/themes_common.mk
@@ -0,0 +1,9 @@
+# Theme engine
+PRODUCT_PACKAGES += \
+    ThemeChooser \
+    ThemesProvider
+
+PRODUCT_COPY_FILES += \
+       vendor/carbon/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
+       vendor/carbon/config/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml
+
