d0ed6b67df98bb762031fb8f413ce1f515a69696
Ricardo Cerqueira <ricardo@cyngn.com>
2015-03-16 12:22:11 +0000

    stelix - Remove userinit from user builds
    /data is supposed to be inaccessible to begin with, and we don't
    want the init sequence tampered with for user builds
    
    Change-Id: If3eb5573248956a14ad56516a9871758f4c00989
    (cherry picked from commit a9d772166365a27181a7441c10a6c60a2bab7dab)
    (cherry picked from commit 4c8f8aba9e549073c8cc50077c6e8931f8dbd8ca)
    
    Signed-off-by: STELIX <ssspinni@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -80,9 +80,11 @@ PRODUCT_COPY_FILES += \
     vendor/stelix/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
     vendor/stelix/prebuilt/common/bin/sysinit:system/bin/sysinit
 
+ifneq ($(TARGET_BUILD_VARIANT),user)
 # userinit support
 PRODUCT_COPY_FILES += \
     vendor/stelix/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit
+endif
 
 # Carbon-specific init file
 PRODUCT_COPY_FILES += \
