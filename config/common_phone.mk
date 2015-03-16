c515c047547bf27a72ec6080200d526ae29c39b9
Abhisek Devkota <ciwrl@cyanogenmod.com>
2015-03-16 11:27:10 -0700

    STELIX - Fix default Alarm tone
    Change-Id: I051350fc82f6167a21a2365adf5309c4fe197859
    Signed-off-by: STELIX <ssspinni@gmail.com>

--- config/common_phone.mk
+++ config/common_phone.mk
@@ -3,7 +3,7 @@ $(call inherit-product, vendor/stelix/config/common.mk)
 
 # ringtones
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.ringtone=Hydra.ogg \
+    ro.config.ringtone=Helium.ogg \
     ro.config.notification_sound=Proxima.ogg \
     ro.config.alarm_alert=Cesium.ogg
 
