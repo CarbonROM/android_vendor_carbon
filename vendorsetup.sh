3f103722499290f96bfdfa24a2de89134cd638bb
Patrick Campanale <patrickappcreator@gmail.com>
2015-05-09 10:22:04 -0500

    Added hltetmo to vendor

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,5 @@
 add_lunch_combo carbon_bacon-userdebug
 add_lunch_combo carbon_flo-userdebug
 add_lunch_combo carbon_hammerhead-userdebug
+add_lunch_combo carbon_hltetmo-userdebug
 add_lunch_combo carbon_m7-userdebug
