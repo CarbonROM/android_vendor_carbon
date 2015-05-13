3e2b8af4042860d769d0e56f9a7e407fc20dd929
Adnan Begovic <adnan@cyngn.com>
2015-05-13 03:57:05 -0700

    cm: Ignore filters for platform library.
    Change-Id: Iaf06872676e9e2745cb2f5ea7dcf1caf71443cb6

--- config/common.mk
+++ config/common.mk
@@ -144,13 +144,11 @@ PRODUCT_PACKAGES += \
     Eleven \
     LockClock
 
-ifeq ($(filter NIGHTLY SNAPSHOT EXPERIMENTAL,$(CM_BUILDTYPE)),)
 # CM Platform Library
 PRODUCT_PACKAGES += \
     org.cyanogenmod.platform-res \
     org.cyanogenmod.platform \
     org.cyanogenmod.platform.xml
-endif
 
 # CM Hardware Abstraction Framework
 PRODUCT_PACKAGES += \
