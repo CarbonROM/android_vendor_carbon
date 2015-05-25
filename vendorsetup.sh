6d0025304f997494a505c99e544122953e4feb1a
STELIX <ssspinni@gmail.com>
2015-05-25 16:02:44 +0200

    carbon - invite some "new" devices
    Change-Id: I0accfa843ee20fec79121495febf0e357089f772
    Signed-off-by: STELIX <ssspinni@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,5 +3,10 @@ add_lunch_combo carbon_flo-userdebug
 add_lunch_combo carbon_hammerhead-userdebug
 add_lunch_combo carbon_hltetmo-userdebug
 add_lunch_combo carbon_m7-userdebug
+add_lunch_combo carbon_m7spr-userdebug
 add_lunch_combo carbon_m8-userdebug
+add_lunch_combo carbon_mako-userdebug
 add_lunch_combo carbon_peregrine-userdebug
+add_lunch_combo carbon_sirius-userdebug
+add_lunch_combo carbon_z3-userdebug
+add_lunch_combo carbon_z3c-userdebug
