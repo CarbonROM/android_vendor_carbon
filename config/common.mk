ab898d7ac49bc6175d1121435b82eb20ad8e7091
Adnan Begovic <adnan@cyngn.com>
2015-07-13 16:16:55 -0700

    vendor/carbon: Bump cmsdk version.
    Change-Id: Id1690423ea1754558119209390c24bfb1270920f

--- config/common.mk
+++ config/common.mk
@@ -313,6 +313,16 @@ ro.romstats.version=$(CARBON_VERSION)
 # by default, do not update the recovery with system updates
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.recovery_update=false
 
+ifndef CM_PLATFORM_SDK_VERSION
+  # This is the canonical definition of the SDK version, which defines
+  # the set of APIs and functionality available in the platform.  It
+  # is a single integer that increases monotonically as updates to
+  # the SDK are released.  It should only be incremented when the APIs for
+  # the new release are frozen (so that developers don't write apps against
+  # intermediate builds).
+  CM_PLATFORM_SDK_VERSION := 2
+endif
+
 # Include CM audio files
 include vendor/carbon/config/cm_audio.mk
 
