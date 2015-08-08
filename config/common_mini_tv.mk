b95a7e567a17f56f31b401f451c2f66fb5a48d1c
Myself5 <myself5@carbonrom.org>
2015-08-08 21:18:15 +0200

    replace wrong vendor path
    Change-Id: I09b5578dcc7d9d73d17cf53066c64369e8aa645a

--- config/common_mini_tv.mk
+++ config/common_mini_tv.mk
@@ -1,8 +1,8 @@
 # Inherit common CM stuff
-$(call inherit-product, vendor/cm/config/common.mk)
+$(call inherit-product, vendor/carbon/config/common.mk)
 
 # Include CM audio files
-include vendor/cm/config/cm_audio.mk
+include vendor/carbon/config/cm_audio.mk
 
 # Default notification/alarm sounds
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -11,5 +11,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
     PRODUCT_COPY_FILES += \
-        vendor/cm/prebuilt/common/bootanimation/800.zip:system/media/bootanimation.zip
+        vendor/carbon/prebuilt/common/bootanimation/800.zip:system/media/bootanimation.zip
 endif
