b659d8a2cae01bf2ad8110254df79335ca14dbee
Myself5 <myself5@carbonrom.org>
2015-10-05 20:52:08 +0200

    vendor: Bringup of the Sony Shinano Lineup based off Sonys AOSP device trees
    Change-Id: Icea2550141d22e12441ba91a7901a2976283b601

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,11 @@
+add_lunch_combo carbon_aries_aosp-userdebug
 add_lunch_combo carbon_bacon-userdebug
 add_lunch_combo carbon_e975-userdebug
 add_lunch_combo carbon_flo-userdebug
 add_lunch_combo carbon_hammerhead-userdebug
 add_lunch_combo carbon_hltetmo-userdebug
 add_lunch_combo carbon_i9100-userdebug
+add_lunch_combo carbon_leo_aosp-userdebug
 add_lunch_combo carbon_m7-userdebug
 add_lunch_combo carbon_m7spr-userdebug
 add_lunch_combo carbon_m8-userdebug
@@ -12,6 +14,7 @@ add_lunch_combo carbon_oneplus2-userdebug
 add_lunch_combo carbon_peregrine-userdebug
 add_lunch_combo carbon_shamu-userdebug
 add_lunch_combo carbon_sirius-userdebug
+add_lunch_combo carbon_sirius_aosp-userdebug
 add_lunch_combo carbon_w7-userdebug
 add_lunch_combo carbon_z3-userdebug
 add_lunch_combo carbon_z3c-userdebug
