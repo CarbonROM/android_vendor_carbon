3c861518d685cab683f0be76d68550a9d5fab5ee
Christian Oder <myself5@carbonrom.org>
2016-01-20 14:29:08 +0100

    vendorsetup: Add sirius back in the game
    Change-Id: I2ab70463591620c6450951bdac08bd8dd8083a5e

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,5 @@
 add_lunch_combo carbon_aries-userdebug
 add_lunch_combo carbon_leo-userdebug
 add_lunch_combo carbon_shamu-userdebug
+add_lunch_combo carbon_sirius-userdebug
 add_lunch_combo carbon_sumire-userdebug
