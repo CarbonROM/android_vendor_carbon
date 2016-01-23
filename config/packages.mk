0d93db674f323c76a2de61b38dfdf664604d2b08
Christian Oder <myself5@carbonrom.org>
2016-01-23 16:59:53 +0100

    vendor: build exFat tools
    Change-Id: I36cca858368c13a5d33ec4a954cf644aab0b82c0

--- config/packages.mk
+++ config/packages.mk
@@ -2,3 +2,12 @@
 
 # Additional apps
 # PRODUCT_PACKAGES += \
+
+WITH_EXFAT ?= true
+ifeq ($(WITH_EXFAT),true)
+TARGET_USES_EXFAT := true
+PRODUCT_PACKAGES += \
+    mount.exfat \
+    fsck.exfat \
+    mkfs.exfat
+endif
