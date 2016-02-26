280e7eda2f123b39a97230927feb07f254a1b36f
maxwen <max.weninger@gmail.com>
2016-02-26 03:45:57 +0100

    [1/2] vendor: sepolicy: external sdcard rodeo
    sepolicy changes required
    and add all ntfs and exfat tools to build
    big thanks to CM
    
    Change-Id: I6b6581b4d6a9fc3ad200c4a6c63e265521f9859d

--- config/packages.mk
+++ config/packages.mk
@@ -5,11 +5,11 @@ PRODUCT_PACKAGES += \
     Chromium \
     VanillaMusic
 
-WITH_EXFAT ?= true
-ifeq ($(WITH_EXFAT),true)
 TARGET_USES_EXFAT := true
 PRODUCT_PACKAGES += \
     mount.exfat \
     fsck.exfat \
-    mkfs.exfat
-endif
+    mkfs.exfat \
+		mkfs.ntfs \
+    mount.ntfs \
+    fsck.ntfs
