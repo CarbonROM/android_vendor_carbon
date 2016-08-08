5fae1f073b87d2738b3d375d075bc12024a14037
Steve Kondik <steve@cyngn.com>
2016-08-07 23:21:29 -0700

    cm: Add custom off-mode charging screen
      * Moving this to open-source from CyanogenOS :)
      * Thanks to Chao Chen, Ethan Chen, Pat Erley, Scott Mertz,
        and Keith Mok for various contributions to this.
    
    Change-Id: If79f9c279668d14ee0cde62889bb09e7185ef08c

--- config/packages.mk
+++ config/packages.mk
@@ -20,3 +20,12 @@ PRODUCT_PACKAGES += \
     OmniStyle \
     VanillaMusic \
     CMUpdater
+
+# Custom off-mode charger
+ifneq ($(WITH_CM_CHARGER),false)
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    cm_charger_res_images \
+    font_log.png \
+    libhealthd.cm
+endif
