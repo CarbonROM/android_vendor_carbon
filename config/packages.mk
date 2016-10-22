4f3bf4b28a5d069bb59d9e735a7a00dd7cd538f6
Christian Oder <myself5@carbonrom.org>
2016-10-22 17:23:28 +0200

    Revert "[1/2] vendor: sepolicy: external sdcard rodeo"
    This reverts commit 280e7eda2f123b39a97230927feb07f254a1b36f.
    
    Change-Id: Ia471f20f2eb6c7cd44c9928aafac40793e0ec714

--- config/packages.mk
+++ config/packages.mk
@@ -5,12 +5,3 @@ PRODUCT_PACKAGES += \
     Chromium \
     masquerade \
     VanillaMusic
-
-TARGET_USES_EXFAT := true
-PRODUCT_PACKAGES += \
-    mount.exfat \
-    fsck.exfat \
-    mkfs.exfat \
-    mkfs.ntfs \
-    mount.ntfs \
-    fsck.ntfs
