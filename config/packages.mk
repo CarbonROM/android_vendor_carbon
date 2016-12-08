44278f600c8bb5dd0028522ee5ed6a78eaec2a10
Alexander Martinz <eviscerationls@gmail.com>
2016-12-08 20:11:20 +0100

    carbon: Fix Trusted Face
    Trusted Face depends on libprotobuf-cpp-full.
    Some devices already ship it (eg RIL needs it).
    
    Include it here to build it for every device.
    
    Link to OpenGapps issue:
    https://github.com/opengapps/opengapps/issues/390
    
    Change-Id: Iffee0aa6218dfef45f5cd728a999fa1114c0dd57
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- config/packages.mk
+++ config/packages.mk
@@ -1,5 +1,10 @@
 # Additional packages
 
+# Include explicitly to work around GMS issues
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full \
+    librsjni
+
 # Additional apps
 PRODUCT_PACKAGES += \
     Launcher3 \
