28e5ca118af04c23e5cfe9934d6bb4a29da2063a
Christian Oder <myself5@carbonrom.org>
2017-01-01 16:03:27 +0100

    vendor: add user targets
    Change-Id: I8429b72a833fc4ae28e3cc45c27a274291ed2aa3

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,3 +7,13 @@ add_lunch_combo carbon_shamu-userdebug
 add_lunch_combo carbon_shieldtablet-userdebug
 add_lunch_combo carbon_sirius-userdebug
 add_lunch_combo carbon_sumire-userdebug
+add_lunch_combo carbon_angler-user
+add_lunch_combo carbon_aries-user
+add_lunch_combo carbon_leo-user
+add_lunch_combo carbon_marlin-user
+add_lunch_combo carbon_sailfish-user
+add_lunch_combo carbon_shamu-user
+add_lunch_combo carbon_shieldtablet-user
+add_lunch_combo carbon_sirius-user
+add_lunch_combo carbon_sumire-user
+
