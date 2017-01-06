d7eecf31c7d5602d825a004afdf401c272afce2b
Christian Oder <myself5@carbonrom.org>
2017-01-06 02:05:39 +0100

    vendor: add mako
    Change-Id: Iaccc28db3eb3b526b5a731799e9be1aa54da1034

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,6 +2,7 @@ add_lunch_combo carbon_angler-userdebug
 add_lunch_combo carbon_aries-userdebug
 add_lunch_combo carbon_castor_windy-userdebug
 add_lunch_combo carbon_leo-userdebug
+add_lunch_combo carbon_mako-userdebug
 add_lunch_combo carbon_marlin-userdebug
 add_lunch_combo carbon_sailfish-userdebug
 add_lunch_combo carbon_shamu-userdebug
@@ -12,10 +13,10 @@ add_lunch_combo carbon_angler-user
 add_lunch_combo carbon_aries-user
 add_lunch_combo carbon_castor_windy-user
 add_lunch_combo carbon_leo-user
+add_lunch_combo carbon_mako-user
 add_lunch_combo carbon_marlin-user
 add_lunch_combo carbon_sailfish-user
 add_lunch_combo carbon_shamu-user
 add_lunch_combo carbon_shieldtablet-user
 add_lunch_combo carbon_sirius-user
 add_lunch_combo carbon_sumire-user
-
