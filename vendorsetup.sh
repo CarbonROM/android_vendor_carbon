b4fd03c098d00ffe066d185aef5a3f55813f666f
Christian Oder <myself5@carbonrom.org>
2017-01-07 05:04:39 +0100

    vendor: Add bullhead
    Change-Id: Id746464ea3ed4808a60414741d950e132f975853

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,6 @@
 add_lunch_combo carbon_angler-userdebug
 add_lunch_combo carbon_aries-userdebug
+add_lunch_combo carbon_bullhead-userdebug
 add_lunch_combo carbon_castor_windy-userdebug
 add_lunch_combo carbon_leo-userdebug
 add_lunch_combo carbon_mako-userdebug
@@ -11,6 +12,7 @@ add_lunch_combo carbon_sirius-userdebug
 add_lunch_combo carbon_sumire-userdebug
 add_lunch_combo carbon_angler-user
 add_lunch_combo carbon_aries-user
+add_lunch_combo carbon_bullhead-user
 add_lunch_combo carbon_castor_windy-user
 add_lunch_combo carbon_leo-user
 add_lunch_combo carbon_mako-user
