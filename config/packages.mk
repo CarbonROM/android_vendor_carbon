9d1d9d29cc2ec3ce0cab51ca424b94931abe3bfb
Christian Oder <myself5@carbonrom.org>
2017-01-17 13:39:01 +0100

    vendor: build telephony-ext
    Change-Id: Ibb7b37e2c257d5cc538677819515dd07c17e5170

--- config/packages.mk
+++ config/packages.mk
@@ -5,6 +5,13 @@ PRODUCT_PACKAGES += \
     libprotobuf-cpp-full \
     librsjni
 
+# Telephony
+PRODUCT_PACKAGES += \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 # Additional apps
 PRODUCT_PACKAGES += \
     Launcher3 \
