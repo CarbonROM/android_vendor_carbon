9399e5791896cf4eeb2c8d18f8a092ca1e7e86a1
Christian Oder <myself5@carbonrom.org>
2017-03-01 18:49:44 +0100

    Revert "vendor: add protective Android.mk for caf/telephony"
    This reverts commit d56a76b53600db7e2d5ba78150d3a9de82519633.
    
    Change-Id: I8bc39267242137cb8d4ca667440fcd628555aac3

--- tools/pixelfix/Android.mk
+++
@@ -1,9 +0,0 @@
-# This Makefile is preventing the Pixels
-# from building the CAF code, because they
-# have their own implementation
-
-ifeq ($(filter marlin sailfish, $(TARGET_DEVICE)),)
-LOCAL_PATH := $(call my-dir)
-
-include $(call all-makefiles-under,$(LOCAL_PATH))
-endif
