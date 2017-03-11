a3ffed98895470f0e298642495497556ffae68ea
gtpitch <bmontagu@gmail.com>
2017-03-11 04:19:53 +0000

    add oneplus2
    Change-Id: I6b75b4c59261a5ca80a6f2cb20cbb250ace26ff8
    Signed-off-by: gtpitch <bmontagu@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,6 +7,7 @@ add_lunch_combo carbon_kiwi-userdebug
 add_lunch_combo carbon_leo-userdebug
 add_lunch_combo carbon_mako-userdebug
 add_lunch_combo carbon_marlin-userdebug
+add_lunch_combo carbon_oneplus2-userdebug
 add_lunch_combo carbon_sailfish-userdebug
 add_lunch_combo carbon_shamu-userdebug
 add_lunch_combo carbon_shieldtablet-userdebug
@@ -21,6 +22,7 @@ add_lunch_combo carbon_kiwi-user
 add_lunch_combo carbon_leo-user
 add_lunch_combo carbon_mako-user
 add_lunch_combo carbon_marlin-user
+add_lunch_combo carbon_oneplus2-user
 add_lunch_combo carbon_sailfish-user
 add_lunch_combo carbon_shamu-user
 add_lunch_combo carbon_shieldtablet-user
