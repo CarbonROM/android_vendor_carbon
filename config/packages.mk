dbcf6880741658d2e6d968451ddf9ba5047b8234
Christian Oder <myself5@carbonrom.org>
2017-05-27 16:05:51 +0200

    carbon: build ffmpeg
    Change-Id: Ia9f3d7c2ca805c9da3647bbc38a4b83292d7f37a

--- config/packages.mk
+++ config/packages.mk
@@ -47,3 +47,13 @@ PRODUCT_PACKAGES += \
     fsck.exfat \
     mkfs.exfat
 endif
+
+# Stagefright FFMPEG plugin
+PRODUCT_PACKAGES += \
+    libffmpeg_extractor \
+    libffmpeg_omx \
+    media_codecs_ffmpeg.xml
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    media.sf.omx-plugin=libffmpeg_omx.so \
+    media.sf.extractor-plugin=libffmpeg_extractor.so
