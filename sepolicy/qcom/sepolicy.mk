fe159669cbbf30deda0591ea0c30d57d135f88f9
Christian Oder <myself5@carbonrom.org>
2017-08-25 23:58:01 +0200

    seplicy: nuke for now, readd later when needed
    Change-Id: I116b5fed0ca4d0015c74cdcb214f4a2b4353e713

--- sepolicy/qcom/sepolicy.mk
+++
@@ -1,7 +0,0 @@
-#
-# This policy configuration will be used by all qcom products that
-# inherit from Carbon
-#
-
-BOARD_SEPOLICY_DIRS += \
-    vendor/carbon/sepolicy/qcom
--- sepolicy/sepolicy.mk
+++
@@ -1,7 +0,0 @@
-#
-# This policy configuration will be used by all products that
-# inherit from Carbon
-#
-
-BOARD_SEPOLICY_DIRS += \
-    vendor/carbon/sepolicy
