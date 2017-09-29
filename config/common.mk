8ac738e99cd04c7e80d11eee486f40f8d068f85a
Christian Oder <myself5@carbonrom.org>
2017-09-29 22:48:51 +0200

    vendor: use SDClang 4.0
    Change-Id: I3cafca38b0684078fe88365afeb18fc5d3c9fe33

--- config/common.mk
+++ config/common.mk
@@ -88,7 +88,7 @@ SQUISHER_SCRIPT := vendor/carbon/tools/squisher
 
 # Include SDCLANG definitions if it is requested and available
 ifeq ($(HOST_OS),linux)
-    ifneq ($(wildcard vendor/qcom/sdclang-3.8/),)
+    ifneq ($(wildcard vendor/qcom/sdclang-4.0/),)
         include vendor/carbon/sdclang/sdclang.mk
     endif
 endif
