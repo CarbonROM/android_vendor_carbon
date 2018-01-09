e19208cf7d367415ac652b6715db53a38815bb11
Sam Mortimer <sam@mortimer.me.uk>
2018-01-09 01:02:17 -0800

    carbon: allow tethering without a provisioning app
    Change-Id: Iac67426229aff842f536301edd9ea270d5d3db6e

--- config/gsm.mk
+++ config/gsm.mk
@@ -14,3 +14,8 @@ PRODUCT_PACKAGES += \
     messaging \
     Stk \
     CellBroadcastReceiver
+
+# Tethering - allow without requiring a provisioning app
+# (for devices that check this)
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    net.tethering.noprovisioning=true
