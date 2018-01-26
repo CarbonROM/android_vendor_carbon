3c17fb8f12daf0c4ae17630cfe4f5fcb1e05108b
Ethan Chen <intervigil@gmail.com>
2018-01-26 09:48:19 -0800

    bfqio: Add missing include header
    Change-Id: I50851e633c54cd0e2e37789e88a226aaa80e8c2c
    Signed-off-by: Joe Maples <joe@frap129.org>

--- libbfqio/bfqio.c
+++ libbfqio/bfqio.c
@@ -23,6 +23,7 @@
 #include <pthread.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
 static int __rtio_cgroup_supported = -1;
 static pthread_once_t __rtio_init_once = PTHREAD_ONCE_INIT;
