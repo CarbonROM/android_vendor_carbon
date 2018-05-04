ab4166f7dbc1897fb10ac3a8a5b964132f6b0726
VenkatVishalV <venkatvishal124@gmail.com>
2018-05-04 08:25:19 +0000

    vendor: add tissot combo to vendorsetup.sh
    Change-Id: Iff009ea823a6e0e0453506f673cfe66da0da313f
    Signed-off-by: VenkatVishalV <venkatvishal124@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -84,6 +84,8 @@ add_lunch_combo carbon_sumire-user
 add_lunch_combo carbon_sumire-userdebug
 add_lunch_combo carbon_taimen-user
 add_lunch_combo carbon_taimen-userdebug
+add_lunch_combo carbon_tissot-user
+add_lunch_combo carbon_tissot-userdebug
 add_lunch_combo carbon_z3-user
 add_lunch_combo carbon_z3-userdebug
 add_lunch_combo carbon_z3c-user
