41e470d79dcf7a5310b7f3322d4966fb8a8ebdab
Michael Benedict <michaelbt@live.com>
2018-06-07 20:50:07 +1000

    Revert "vendor: Add TARGET_OMX_LEGACY_RESCALING flag"
    This reverts commit 595d8dc2bf07bbecefe8b9ef32ea666ea808822e.
    
    Change-Id: Iadaceae494fd465ba05a23971f69b0d224936968
    Signed-off-by: Michael Benedict <michaelbt@live.com>

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -27,9 +27,6 @@ type Product_variables struct {
 	Qti_flac_decoder struct {
 		Cflags []string
 	}
-	Use_legacy_rescaling struct {
-		Cflags []string
-	}
 
 	Uses_generic_camera_parameter_library struct {
 		Srcs []string
@@ -65,7 +62,6 @@ type ProductVariables struct {
 	Cant_reallocate_omx_buffers *bool `json:",omitempty"`
 	Qcom_bsp_legacy         *bool `json:",omitempty"`
 	Qti_flac_decoder        *bool `json:",omitempty"`
-	Use_legacy_rescaling    *bool `json:",omitempty"`
 	Target_uses_qsml      *bool `json:",omitempty"`
 	Target_uses_eigen       *bool `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
