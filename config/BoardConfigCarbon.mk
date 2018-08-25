3cf51800821db2a5050540cb667c6e99ed96ad6a
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-25 11:10:25 -0700

    carbon: Move QC board variables earlier
    These variables are usually set in a device's BoardConfig, setting them
    in qcom_target is at the very end of the "configuration process" which
    results in them being unavailable to plenty of other configuration
    "things" (ex. soong namespaces or soong config). Move them to right
    after a device's BoardConfig has been found and loaded.
    
    Change-Id: Iddd731202d22ed3f8eb010197ce20d3c75a1f40a

--- config/BoardConfigCarbon.mk
+++ config/BoardConfigCarbon.mk
@@ -2,3 +2,11 @@
 ifneq ($(WITH_CM_CHARGER),false)
     BOARD_HAL_STATIC_LIBRARIES := libhealthd.cm
 endif
+
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+    BOARD_USES_QTI_HARDWARE := true
+endif
+
+ifeq ($(BOARD_USES_QTI_HARDWARE),true)
+include vendor/carbon/config/BoardConfigQcom.mk
+endif
