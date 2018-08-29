861a91f061339a57f54d0ff749e90bf651a716f6
Kevin F. Haggerty <haggertk@lineageos.org>
2018-08-28 18:22:25 -0600

    soong_config: Add add_json_str_omitempty function
    * This function wraps add_json_str so that it does not add the soong
      variable key:value pair to json structure if the second argument
      (value) is empty/unset, preventing the need for more complex
      conditional logic in the middle the code that builds the struct
    * This concept allows us to make use of the omitempty field option
      for purposes like single variables controlling ifdef blocks as well
      as carrying the desired value
    
    Change-Id: I99c8c162069c2aa8ff3d0bab2636b01181e74a9d

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,3 +1,5 @@
+add_json_str_omitempty = $(if $(strip $(2)),$(call add_json_str, $(1), $(2)))
+
 $(eval _contents := $$(_contents)"Carbon": {$$(newline))
 
 $(call add_json_bool,	Has_legacy_camera_hal1,						$(filter true,$(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false)))
