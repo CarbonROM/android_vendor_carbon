a5b44305a16088efec391792794361b178665e7a
Danny Baumann <dannybaumann@web.de>
2018-08-29 11:04:58 +0200

    soong_config: Allow process-specific override of target SDK version
    Change-Id: I775e852c17af12d2233e644e3db10c10bcb20d19

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -19,8 +19,10 @@ $(call add_json_bool,	Uses_qcom_um_4_4_family, 					$(filter true,$(TARGET_USES_
 $(call add_json_bool,	Uses_qcom_um_4_9_family, 					$(filter true,$(TARGET_USES_QCOM_UM_4_9_FAMILY)))
 $(call add_json_bool,	Uses_qcom_um_4_14_family, 					$(filter true,$(TARGET_USES_QCOM_UM_4_14_FAMILY)))
 $(call add_json_bool,	Has_legacy_camera_hal1,						$(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)))
+$(call add_json_bool,	Supports_extended_compress_format,			$(filter true,$(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)))
 
 $(call add_json_str_omitempty,	Additional_gralloc_10_usage_bits,	$(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
+$(call add_json_str_omitempty,	Target_process_sdk_version_override,$(TARGET_PROCESS_SDK_VERSION_OVERRIDE))
 
 $(call add_json_val_default,	Bootloader_message_offset,			$(BOOTLOADER_MESSAGE_OFFSET), 0)
 
@@ -30,7 +32,6 @@ $(call add_json_str,	QTIDisplayPath,								$(call project-path-for,qcom-display
 $(call add_json_str,	QTIMediaPath,								$(call project-path-for,qcom-media))
 $(call add_json_str,	Specific_camera_parameter_library,			$(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY))
 $(call add_json_str,	Target_shim_libs,							$(subst $(space),:,$(TARGET_LD_SHIM_LIBS)))
-$(call add_json_bool, Supports_extended_compress_format, $(filter true,$(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _json_contents := $(_json_contents)__SV_END
