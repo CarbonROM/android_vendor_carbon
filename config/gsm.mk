c90762bf515958fd69ca3176209ad3c6f5b0d3e3
Rashed Abdel-Tawab <rashed@linux.com>
2018-09-06 08:48:26 -0700

    carbon: Dynamically add custom APNs
    For Example new devices need the new Sprint APNs, so add a new board flag for
    them to set that will make the build system add the new APNs to
    the APN config XML. If the flag is not set, continue to use the
    old APNs.
    To add or replace APNs, the custom_apns.py script has been
    added. If CUSTOM_APNS_FILE is defined, custom_apns.py
    is run and generates a new version of apns-conf.xml.
    
    Change-Id: I7ff12a4342de2a7663b2b66fd627244214a8dc71

--- config/gsm.mk
+++ config/gsm.mk
@@ -3,8 +3,8 @@ PRODUCT_COPY_FILES += \
     vendor/carbon/prebuilt/etc/sensitive_pn.xml:system/etc/sensitive_pn.xml
 
 # GSM APN list
-PRODUCT_COPY_FILES += \
-    vendor/carbon/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+PRODUCT_PACKAGES += \
+    apns-conf.xml
 
 PRODUCT_COPY_FILES += \
     vendor/carbon/prebuilt/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
