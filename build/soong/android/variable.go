8b4cf48a61644bc91fe562d06a9ffcb69354ba28
Christian Oder <myself5@carbonrom.org>
2019-01-13 23:26:04 +0100

    vendor: cleanup and sort soong variables
    Change-Id: I09e1d54213e47b43b1f38d7ef8db62a64d92b8fa

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,43 +1,16 @@
 package android
 type Product_variables struct {
-	Additional_gralloc_10_usage_bits struct {
-		Cppflags []string
-	}
-
-	Has_legacy_camera_hal1 struct {
-		Cflags []string
-	}
-
-	Needs_legacy_camera_hal1_dyn_native_handle struct {
-		Cppflags []string
-	}
-
-	Uses_media_extensions struct {
-		Cflags []string
-	}
-
 	Needs_text_relocations struct {
 		Cppflags []string
-	}
-
-	Mtk_hardware struct {
-		Cflags []string
-	}
-
-	Cant_reallocate_omx_buffers struct {
-		Cflags []string
 	}
 
 	Qcom_bsp_legacy struct {
 		Cppflags []string
 	}
 
-	Qti_flac_decoder struct {
-		Cflags []string
-	}
-
-	Uses_generic_camera_parameter_library struct {
-		Srcs []string
+	Target_uses_eigen struct {
+		Shared_libs []string
+		Required []string
 	}
 
 	Target_uses_qsml struct {
@@ -47,9 +20,12 @@ type Product_variables struct {
 		Required []string
 	}
 
-	Target_uses_eigen struct {
-		Shared_libs []string
-		Required []string
+	Uses_generic_camera_parameter_library struct {
+		Srcs []string
+	}
+
+	Additional_gralloc_10_usage_bits struct {
+		Cppflags []string
 	}
 
 	Target_shim_libs struct {
@@ -58,23 +34,18 @@ type Product_variables struct {
 }
 
 type ProductVariables struct {
-	Additional_gralloc_10_usage_bits  			*string `json:",omitempty"`
-	Has_legacy_camera_hal1						*bool `json:",omitempty"`
-	Java_Source_Overlays						*string `json:",omitempty"`
-	Needs_legacy_camera_hal1_dyn_native_handle	*bool `json:",omitempty"`
-	Uses_media_extensions						*bool `json:",omitempty"`
-	Uses_generic_camera_parameter_library		*bool `json:",omitempty"`
-	Specific_camera_parameter_library			*string `json:",omitempty"`
 	Needs_text_relocations						*bool `json:",omitempty"`
-	Mtk_hardware								*bool `json:",omitempty"`
+	Qcom_bsp_legacy								*bool `json:",omitempty"`
+	Target_uses_eigen							*bool `json:",omitempty"`
+	Target_uses_qsml							*bool `json:",omitempty"`
+	Uses_generic_camera_parameter_library		*bool `json:",omitempty"`
+	
+	Additional_gralloc_10_usage_bits  			*string `json:",omitempty"`
+
+	Java_Source_Overlays						*string `json:",omitempty"`
 	QTIAudioPath								*string `json:",omitempty"`
 	QTIDisplayPath								*string `json:",omitempty"`
 	QTIMediaPath								*string `json:",omitempty"`
-	Cant_reallocate_omx_buffers					*bool `json:",omitempty"`
-	Qcom_bsp_legacy								*bool `json:",omitempty"`
-	Qti_flac_decoder							*bool `json:",omitempty"`
-	Target_uses_qsml							*bool `json:",omitempty"`
-	Target_uses_eigen							*bool `json:",omitempty"`
-	Target_use_sdclang							*bool `json:",omitempty"`
+	Specific_camera_parameter_library			*string `json:",omitempty"`
 	Target_shim_libs							*string `json:",omitempty"`
 }
