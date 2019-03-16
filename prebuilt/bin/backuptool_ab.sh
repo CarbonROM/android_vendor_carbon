55d0ddec595d1074c775c700aa170f69cad75310
Christian Oder <myself5@carbonrom.org>
2019-03-17 00:31:36 +0100

    backuptool_ab: Move to Carbon Versioning
    Change-Id: I34e4076c928819b52357dbe9b4c996e28fe25add

--- prebuilt/bin/backuptool_ab.sh
+++ prebuilt/bin/backuptool_ab.sh
@@ -5,7 +5,7 @@
 
 export S=/system
 export C=/postinstall/tmp/backupdir
-export V=8.1
+export V=CR-7
 
 export ADDOND_VERSION=2
 
@@ -50,7 +50,7 @@ if [ ! -r /system/build.prop ]; then
     return 0
 fi
 
-if ( ! grep -q "^ro.build.version.release=$V.*" /system/build.prop ); then
+if ( ! grep -q "^ro.carbon.version=$V.*" /system/build.prop ); then
   echo "Not backing up files from incompatible version: $V"
   return 0
 fi
