5994f736034c005f365773adf946d9ed9fd1ae1f
Alessandro Astone <ales.astone@gmail.com>
2019-09-06 01:13:17 +0200

    carbon: soong: _contents -> _json_contents
    Change-Id: I2baa36bbb9aff3a7e70a293ed3352fb718eb611c

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,6 +1,6 @@
 add_json_str_omitempty = $(if $(strip $(2)),$(call add_json_str, $(1), $(2)))
 
-$(eval _contents := $$(_contents)"Carbon": {$$(newline))
+_json_contents := $(_json_contents)    "Carbon":{$(newline)
 
 $(call add_json_bool,	Needs_text_relocations,					$(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)))
 $(call add_json_bool,	Qcom_bsp_legacy,					$(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)))
@@ -27,4 +27,7 @@ $(call add_json_str,	QTIMediaPath,						$(call project-path-for,qcom-media))
 $(call add_json_str,	Specific_camera_parameter_library,			$(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY))
 $(call add_json_str,	Target_shim_libs,					$(subst $(space),:,$(TARGET_LD_SHIM_LIBS)))
 
-$(eval _contents := $(subst $$(comma)$$(newline)__SV_END,$(newline),$$(_contents)__SV_END},$$(newline)))
+# This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
+_json_contents := $(_json_contents)__SV_END
+
+_json_contents := $(_json_contents)    },$(newline)
