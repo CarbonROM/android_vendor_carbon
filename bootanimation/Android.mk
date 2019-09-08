b6c59a7f49807a6a8be942b3493a1d77e3adc874
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-09-08 22:02:37 +0200

    lineage: Rework bootanimation generation
    Change-Id: Ib0b363c3e0545cb0c86dc69dd1a75f82dd777e59

--- bootanimation/Android.mk
+++ bootanimation/Android.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2016 The CyanogenMod Project
-#               2017 The LineageOS Project
+#               2017-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,18 +15,39 @@
 # limitations under the License.
 #
 
-define build-bootanimation
-    $(shell) vendor/carbon/bootanimation/generate-bootanimation.sh \
-    $(PRODUCT_OUT) \
-    $(TARGET_SCREEN_WIDTH) \
-    $(TARGET_SCREEN_HEIGHT) \
-    $(TARGET_BOOTANIMATION_HALF_RES)
-endef
+ifeq ($(TARGET_SCREEN_WIDTH),)
+    $(warning TARGET_SCREEN_WIDTH is not set, using default value: 1080)
+    TARGET_SCREEN_WIDTH := 1080
+endif
+ifeq ($(TARGET_SCREEN_HEIGHT),)
+    $(warning TARGET_SCREEN_HEIGHT is not set, using default value: 1920)
+    TARGET_SCREEN_HEIGHT := 1920
+endif
 
 TARGET_GENERATED_BOOTANIMATION := $(TARGET_OUT_INTERMEDIATES)/BOOTANIMATION/bootanimation.zip
-$(TARGET_GENERATED_BOOTANIMATION):
-	@echo "Building bootanimation"
-	$(build-bootanimation)
+$(TARGET_GENERATED_BOOTANIMATION): INTERMEDIATES := $(TARGET_OUT_INTERMEDIATES)/BOOTANIMATION
+$(TARGET_GENERATED_BOOTANIMATION): $(SOONG_ZIP)
+	@echo "Building bootanimation.zip"
+	@rm -rf $(dir $@)
+	@mkdir -p $(dir $@)
+	$(hide) tar xfp vendor/carbon/bootanimation/bootanimation.tar -C $(INTERMEDIATES)
+	$(hide) if [ $(TARGET_SCREEN_HEIGHT) -lt $(TARGET_SCREEN_WIDTH) ]; then \
+	    IMAGEWIDTH=$(TARGET_SCREEN_HEIGHT); \
+	else \
+	    IMAGEWIDTH=$(TARGET_SCREEN_WIDTH); \
+	fi; \
+	IMAGESCALEWIDTH=$$IMAGEWIDTH; \
+	if [ "$(TARGET_BOOTANIMATION_HALF_RES)" = "true" ]; then \
+	    IMAGEWIDTH="$$(expr "$$IMAGEWIDTH" / 2)"; \
+	fi; \
+	RESOLUTION="$$IMAGEWIDTH"x"$$IMAGEWIDTH"; \
+	for part_cnt in 0 1 2; do \
+	    mkdir -p $(INTERMEDIATES)/part$$part_cnt; \
+	done; \
+	mogrify -resize $$RESOLUTION -colors 250 $(INTERMEDIATES)/*/*.png; \
+	echo "$$IMAGESCALEWIDTH $$IMAGESCALEWIDTH 30" > $(INTERMEDIATES)/desc.txt; \
+	cat vendor/carbon/bootanimation/desc.txt >> $(INTERMEDIATES)/desc.txt
+	$(hide) $(SOONG_ZIP) -L 0 -o $(TARGET_GENERATED_BOOTANIMATION) -C $(INTERMEDIATES) -D $(INTERMEDIATES)
 
 ifeq ($(TARGET_BOOTANIMATION),)
     TARGET_BOOTANIMATION := $(TARGET_GENERATED_BOOTANIMATION)
@@ -57,5 +78,4 @@ LOCAL_MODULE_PATH := $(TARGET_OUT)/media
 include $(BUILD_SYSTEM)/base_rules.mk
 
 $(LOCAL_BUILT_MODULE): $(TARGET_BOOTANIMATION)
-	@mkdir -p $(dir $@)
 	@cp $(TARGET_BOOTANIMATION) $@
