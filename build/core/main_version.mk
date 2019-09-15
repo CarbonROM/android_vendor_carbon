cb61f4c478c483948a64f315dd87c0dcb6ff019c
Volodymyr Zhdanov <wight554@gmail.com>
2019-09-15 18:54:45 +0300

    build: set build fingerprint for all devices
    * there's no per device way to add props to /system/build.prop
    * adding it here is the only way so far
    
    Change-Id: Iafc286d9e479677540a4ec617db76715c018ae8e

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -5,3 +5,9 @@ ADDITIONAL_BUILD_PROPERTIES += \
     ro.romstats.url=https://stats.carbonrom.org \
     ro.romstats.name=CarbonROM \
     ro.romstats.version=$(CARBON_VERSION)
+
+# Build fingerprint
+ifneq ($(BUILD_FINGERPRINT),)
+ADDITIONAL_BUILD_PROPERTIES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
+endif
