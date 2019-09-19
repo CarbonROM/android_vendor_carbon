d4cd37cab6b289a21c08fc44e38982b48b9d6690
Michael Bestas <mkbestas@lineageos.org>
2019-09-20 01:15:25 +0300

    soong: Add AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT
    Change-Id: Iac6366d714ab3634def648e6a8c0c30d684a9383

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -30,6 +30,7 @@ $(call add_json_str,	QTIDisplayPath,								$(call project-path-for,qcom-display
 $(call add_json_str,	QTIMediaPath,								$(call project-path-for,qcom-media))
 $(call add_json_str,	Specific_camera_parameter_library,			$(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY))
 $(call add_json_str,	Target_shim_libs,							$(subst $(space),:,$(TARGET_LD_SHIM_LIBS)))
+$(call add_json_bool, Supports_extended_compress_format, $(filter true,$(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _json_contents := $(_json_contents)__SV_END
