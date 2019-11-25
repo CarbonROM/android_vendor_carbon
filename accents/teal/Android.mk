5b647e3c7fd661f1b68ad405050f57d94cefd839
Demon Singur <demonsingur@gmail.com>
2019-11-25 17:56:32 +0100

    accents: Redo for compatibility with ThemePicker
    * removed QPurple
    * Added Pixel Blue
    
    Based on AOSP Overlays now
    
    Change-Id: Ia1b91db68df35a4bd58332a95aa4aaeea9b0264e

--- accents/teal/Android.mk
+++ accents/teal/Android.mk
@@ -1,11 +1,13 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
+LOCAL_RRO_THEME := TealAccent
 
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 LOCAL_MODULE_TAGS := optional
-
+LOCAL_PRODUCT_MODULE := true
 LOCAL_PACKAGE_NAME := Teal
 LOCAL_SDK_VERSION := current
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := false
 
-include $(BUILD_PACKAGE)
+include $(BUILD_RRO_PACKAGE)
