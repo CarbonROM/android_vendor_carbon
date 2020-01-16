LineageOS/android_vendor_lineage

Expose Tegra version to blueprint
Change-Id: I8757063b189cf1ae67b7d111179df6cb1c730285

build/soong/soong_config.mk
@@ -16,6 +16,7 @@ $(call add_json_bool, Supports_hw_fde_perf, $(filter true,$(TARGET_HW_DISK_ENCRY
 $(call add_json_bool, Supports_legacy_hw_fde, $(filter true,$(TARGET_LEGACY_HW_DISK_ENCRYPTION)))
 $(call add_json_str_omitempty, Target_process_sdk_version_override, $(TARGET_PROCESS_SDK_VERSION_OVERRIDE))
 $(call add_json_str_omitempty, Target_shim_libs, $(TARGET_LD_SHIM_LIBS))
+$(call add_json_str_omitempty, Target_tegra_version, $(TARGET_TEGRA_VERSION))
 $(call add_json_bool, Uses_generic_camera_parameter_library, $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),,true))
 $(call add_json_bool, Uses_metadata_as_fde_key, $(filter true,$(TARGET_USES_METADATA_AS_FDE_KEY)))
 $(call add_json_bool, Uses_nvidia_enhancements, $(filter true,$(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)))

CarbonROM/android_vendor_carbon

Signed-off-by: RJ Trujillo <certifiedblyndguy@gmail.com>

build/soong/soong_config.mk
@@ -27,5 +27,6 @@ $(call add_json_str,	QTIDisplayPath,						$(call project-path-for,qcom-display))
 $(call add_json_str,	QTIMediaPath,						$(call project-path-for,qcom-media))
 $(call add_json_str,	Specific_camera_parameter_library,			$(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY))
 $(call add_json_str,	Target_shim_libs,					$(subst $(space),:,$(TARGET_LD_SHIM_LIBS)))
+$(call add_json_str,	Target_tegra_version,					$(TARGET_TEGRA_VERSION))
 
 $(eval _contents := $(subst $$(comma)$$(newline)__SV_END,$(newline),$$(_contents)__SV_END},$$(newline)))