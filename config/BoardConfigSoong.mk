69d02346314520070c03fe2fbc6152649aa01fd6
Christian Oder <myself5@carbonrom.org>
2021-05-09 22:50:05 +0200

    carbon: Add configs for QML
    Change-Id: I83b503663d0353bde6ac006dcb242ad4c1281883

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -40,7 +40,9 @@ SOONG_CONFIG_carbonGlobalVars += \
     target_ld_shim_libs \
     target_process_sdk_version_override \
     target_surfaceflinger_fod_lib \
-    uses_camera_parameter_lib
+    uses_camera_parameter_lib \
+    target_uses_qml \
+    target_uses_eigen
 
 SOONG_CONFIG_NAMESPACES += carbonNvidiaVars
 SOONG_CONFIG_carbonNvidiaVars += \
@@ -68,6 +70,8 @@ SOONG_CONFIG_carbonGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAME
 SOONG_CONFIG_carbonGlobalVars_ignores_ftp_pptp_conntrack_failure := $(TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE)
 SOONG_CONFIG_carbonGlobalVars_needs_netd_direct_connect_rule := $(TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE)
 SOONG_CONFIG_carbonGlobalVars_target_enforce_ab_ota_partition_list := $(TARGET_ENFORCE_AB_OTA_PARTITION_LIST)
+SOONG_CONFIG_carbonGlobalVars_target_uses_qml := $(TARGET_USES_QML)
+SOONG_CONFIG_carbonGlobalVars_target_uses_eigen := $(if $(strip $(TARGET_USES_QML)),false,true)
 SOONG_CONFIG_carbonNvidiaVars_uses_nv_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
 SOONG_CONFIG_carbonQcomVars_should_wait_for_qsee := $(TARGET_KEYMASTER_WAIT_FOR_QSEE)
 SOONG_CONFIG_carbonQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
