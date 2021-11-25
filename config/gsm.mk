844f91b335a46eb45bdb1d8cd9a2ba6b9814f25c
Jacob McSwain <jacob@mcswain.dev>
2021-11-24 19:11:18 -0600

    Move a bunch of stuff to appropriate partitions for GSI
    Change-Id: I659a6df7c249c02cb26630b7edfcad0540e0c97d

--- config/gsm.mk
+++ config/gsm.mk
@@ -1,13 +1,10 @@
 # Sensitive Phone Numbers list
 PRODUCT_COPY_FILES += \
-    vendor/carbon/prebuilt/etc/sensitive_pn.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sensitive_pn.xml
+    vendor/carbon/prebuilt/etc/sensitive_pn.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sensitive_pn.xml
 
 # GSM APN list
 PRODUCT_PACKAGES += \
     apns-conf.xml
-
-PRODUCT_COPY_FILES += \
-    vendor/carbon/prebuilt/etc/selective-spn-conf.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/selective-spn-conf.xml
 
 # SIM Toolkit
 PRODUCT_PACKAGES += \
