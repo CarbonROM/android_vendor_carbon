ad48d0a0458048715b56f411dd24232f5fa490e7
Jacob McSwain <jacob@mcswain.dev>
2022-10-11 10:47:31 -0500

    vendor: Remove `telephony-ext` from boot jars until CAF

--- config/packages.mk
+++ config/packages.mk
@@ -19,8 +19,8 @@ PRODUCT_PACKAGES += \
     qti_telephony_utils.xml \
     telephony-ext
 
-PRODUCT_BOOT_JARS += \
-    telephony-ext
+# PRODUCT_BOOT_JARS += \
+#     telephony-ext
 
 # CellBroadcastReceiver
 PRODUCT_PACKAGES += \
