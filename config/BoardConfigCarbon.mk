ade7c1fd9b0f0343a018177de8be53b875322360
Christian Oder <myself5@carbonrom.org>
2022-11-09 17:30:23 +0100

    BoardConfigCarbon: remove long gone includes and configs
    Change-Id: I03049aad9318de03b54cdfcf4a0901cfe426b7f9

--- config/BoardConfigCarbon.mk
+++ config/BoardConfigCarbon.mk
@@ -1,9 +1,6 @@
 # Add our overlays
 DEVICE_PACKAGE_OVERLAYS += vendor/carbon/overlay/common
 
-# Squisher Location
-SQUISHER_SCRIPT := vendor/carbon/tools/squisher
-
 # Versioning
 -include vendor/carbon/config/version.mk
 
@@ -15,7 +12,5 @@ include vendor/carbon/config/BoardConfigKernel.mk
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/carbon/config/BoardConfigQcom.mk
 endif
-
--include vendor/carbon/perf/BoardConfigVendor.mk
 
 include vendor/carbon/config/BoardConfigSoong.mk
