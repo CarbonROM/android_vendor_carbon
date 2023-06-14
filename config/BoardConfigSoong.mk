b7283e9530ce0d2fa25eb86be25c9091ea201322
LuK1337 <priv.luk@gmail.com>
2023-06-14 21:52:43 +0200

    soong: Add `aapt_version_code` default
    This appends `--version_code=$(date -u +%Y%m%d)` to aapt flags, which is
    useful for flushing some caches upon system updates.
    
    Change-Id: I6575b878f09c1c3138e12abc34d39405f51245e7

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -30,6 +30,7 @@ $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
 
 SOONG_CONFIG_NAMESPACES += carbonGlobalVars
 SOONG_CONFIG_carbonGlobalVars += \
+    aapt_version_code \
     additional_gralloc_10_usage_bits \
     gralloc_handle_has_reserved_size \
     bootloader_message_offset \
@@ -97,6 +98,7 @@ TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY ?= libcamera_parameters
 TARGET_SURFACEFLINGER_UDFPS_LIB ?= surfaceflinger_udfps_lib
 
 # Soong value variables
+SOONG_CONFIG_carbonGlobalVars_aapt_version_code := $(shell date -u +%Y%m%d)
 SOONG_CONFIG_carbonGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
 SOONG_CONFIG_carbonGlobalVars_bootloader_message_offset := $(BOOTLOADER_MESSAGE_OFFSET)
 SOONG_CONFIG_carbonGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
